ae5d6be90461eca3b0da81c2526dd9201c00f142
Mikhail <mikhail.mikhalev37@gmail.com>
2015-12-23 00:43:19 +0300

    Изменены роуты на новые

--- app/src/main/java/com/example/mikhail/stockstore/Classes/APIConstants.java
+++ app/src/main/java/com/example/mikhail/stockstore/Classes/APIConstants.java
@@ -13,13 +13,11 @@ public class APIConstants {
     public final static String GET_ALL_COMPANIES = "getallcompanies";
     public final static String GET_ALL_CATEGORIES = "getAllCategories";
 
-    public final static String USER_AUTH_ROUTE = "auth/authorize/user";
-    public final static String USER_REGISTER_ROUTE = "auth/register/user";
-
-    public final static String GET_ALL_STOCKS_ROUTE = "stocks/all";
-
-    public final static String USER_ADD_STOCK_ROUTE = "users/addstock";
-    public final static String USER_GET_FEED_ROUTE = "users/feed";
-    public final static String GET_ALL_COMPANIES_ROUTE = "companies/all";
+    public final static String USER_AUTH_ROUTE = "user/authorize";
+    public final static String USER_REGISTER_ROUTE = "user/register";
+    public final static String GET_ALL_STOCKS_ROUTE = "user/stocks/all";
+    public final static String USER_ADD_STOCK_ROUTE = "user/stocks/subscribe";
+    public final static String USER_GET_FEED_ROUTE = "user/stocks/feed";
+    public final static String GET_ALL_COMPANIES_ROUTE = "user/companies/all";
     public final static String GET_ALL_CATEGORIES_ROUTE = "";
 }
