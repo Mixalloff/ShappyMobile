a865bb89d7c57dbbac84201888dad6f5c7dff2f4
Mikhail <mikhail.mikhalev37@gmail.com>
2016-02-03 14:10:51 +0300

    Добавлены иконки поиска и обработчики в тулбар на странице акций и друзей

--- app/src/main/java/com/example/mikhail/stockstore/StocksActivity.java
+++ app/src/main/java/com/example/mikhail/stockstore/StocksActivity.java
@@ -1,28 +1,43 @@
 package com.example.mikhail.stockstore;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.SearchView;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.mikhail.stockstore.Classes.CommonFunctions;
 
-public class StocksActivity extends ActionBarActivity {
+public class StocksActivity extends ActionBarActivity  implements SearchView.OnQueryTextListener {
+
+    MenuItem searchMenuItem;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stocks);
-
         CommonFunctions.addNavigationView(this,CommonFunctions.setToolbar(this, R.id.toolbar));
         CommonFunctions.addStockTabs(this);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_stocks, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        searchMenuItem = menu.findItem(R.id.action_search);
+        try {
+            searchView = (SearchView) searchMenuItem.getActionView();
+            searchView.setOnQueryTextListener(this);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
         return true;
     }
 
@@ -39,5 +54,17 @@ public class StocksActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Toast.makeText(this, newText, Toast.LENGTH_SHORT).show();
+        return false;
     }
 }
